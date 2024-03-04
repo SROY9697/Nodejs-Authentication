4caf1b60e389ef7a5664bb78d6a18250e5cf65e7
Subhrajit Roy <81874861+SROY9697@users.noreply.github.com>
2024-03-04 17:37:56 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+//------------ Importing Controllers ------------//
+const authController = require("../controllers/authController");
+
+router.get("/login", (req, res) => res.render("login"));
+
+router.get("/forgot", (req, res) => res.render("forgot"));
+
+router.get("/reset/:id", (req, res) => {
+  res.render("reset", { id: req.params.id });
+});
+
+router.get("/register", (req, res) => res.render("register"));
+
+router.post("/register", authController.registerHandle);
+
+router.get("/activate/:token", authController.activateHandle);
+
+router.post("/forgot", authController.forgotPassword);
+
+router.post("/reset/:id", authController.resetPassword);
+
+router.get("/forgot/:token", authController.gotoReset);
+
+router.post("/login", authController.loginHandle);
+
+router.get("/logout", authController.logoutHandle);
+
+module.exports = router;
 
